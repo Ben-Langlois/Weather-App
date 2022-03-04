07131befbf2a96fd6b7c47861939bbd96e14f93f
Ben_Langlois <benlangloiss@gmail.com>
2022-03-03 23:59:40 -0500

    First

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import React, { Component } from 'react';
+import $ from 'jquery';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  handleSubmit = () => {
+    let location = $('#form=control').val();
+
+    $.ajax({
+      url: `http://api.openweathermap.org/geo/1.0/direct?q=${location}&appid=ad46bca0cb15937504da590a8559bbae"`,
+      type: 'GET',
+      success: function (result){
+        
+      },
+      error: function (error){
+
+      }
+    })
+  }
+
+  onRetrieval = (coords) => {
+    $.ajax({
+      url: 'http://api.openweathermap.org/geo/1.0/direct?q={city name}&limit={limit}&appid=ad46bca0cb15937504da590a8559bbae',
+      type: 'GET',
+      success: function (result){
+        
+      },
+      error: function (error){
+
+      }
+    })
+  }
+
+  render(){
+    return (
+      <div>
+        <div class="input-group input-group-sm w-50 mb-3 mx-auto my-5">
+          <div class="input-group-prepend">
+            <span class="input-group-text" id="inputGroup-sizing-sm">Location</span>
+          </div>
+          <input type="text" class="form-control" aria-label="Small" aria-describedby="inputGroup-sizing-sm" placeholder='eg. Toronto, Ontario'></input>
+        </div>  
+      </div>
+    );
+  }
 }
 
 export default App;
